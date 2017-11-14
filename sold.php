37304b706f032d46ce2dc438d6d57231b6869227
dorcas1 <dorcasoyebokun@gmail.com>
2017-11-14 13:01:32 +0000

    reviewed

--- sold.php
+++ sold.php
@@ -1,4 +1,6 @@
 <?php
-echo "this is a file of things sold but its in a branch called bought"
+echo "this is a file of things sold but its in a branch called bought";
+echo "this is reviewed";
+echo "the plan is to branch out on a branch"
 
 ?>
